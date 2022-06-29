d53a94a4c17b43fb93478c09528e31644d829217
devwulff <p.wulff887@gmail.com>
2022-06-29 15:02:29 +0200

    NodesJS - d1 - LvL1_1

---
+++ index.js
@@ -0,0 +1,6 @@
+const { arrayNumbers, arrayCars } = require("./data");
+const { compareArr } = require("./function");
+
+console.log(compareArr(arrayCars));
+
+console.log(compareArr(arrayNumbers));
